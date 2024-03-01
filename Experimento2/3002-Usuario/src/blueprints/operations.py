a42022c6b5dfaf1bde2bee6168389753a199ed5d
David Andres Vargas Leon <da.vargasl1@uniandes.edu.co>
2024-02-29 22:11:33 -0500

    Squashed commit of the following:
    commit 9893572979430f7be75c4c6f09c067370641add8
    Author: Benito Zarate Palomec <b.zarate@uniandes.edu.co>
    Date:   Thu Feb 29 21:10:18 2024 -0600
    
        adecuaciones
    
    commit e4a85c63efe5e59d340f5ae16054c7e4f555fa61
    Author: David Andres Vargas Leon <da.vargasl1@uniandes.edu.co>
    Date:   Thu Feb 29 21:32:15 2024 -0500
    
        añadir consultar servicios en usuario y health en socios
    
    commit 77d044e5a57084d33231d9d96d93935baaa55942
    Author: Benito Zarate Palomec <b.zarate@uniandes.edu.co>
    Date:   Thu Feb 29 20:14:38 2024 -0600
    
        .
    
    commit e2d16942119d7ea1ac907a6f621005e7e91d7dbe
    Author: Benito Zarate Palomec <b.zarate@uniandes.edu.co>
    Date:   Thu Feb 29 20:13:36 2024 -0600
    
        validacion sqs
    
    commit 4d2b57d117c3f664f9f640e5f46fcb73167aaefe
    Author: David Andres Vargas Leon <da.vargasl1@uniandes.edu.co>
    Date:   Thu Feb 29 20:49:15 2024 -0500
    
        partner service modified to save partner and configure main socios
    
    commit 331feb62555c7ef9d0115744f8fc74408fcaf7d6
    Author: David Andres Vargas Leon <da.vargasl1@uniandes.edu.co>
    Date:   Thu Feb 29 09:00:40 2024 -0500
    
        avance socios
    
    commit 3523cb4ef11800e5857e83e1424932880bc50fe9
    Author: David Andres Vargas Leon <da.vargasl1@uniandes.edu.co>
    Date:   Wed Feb 28 21:46:02 2024 -0500
    
        initial configuration service and service to partner

---
+++ Experimento2/3002-Usuario/src/blueprints/operations.py
@@ -0,0 +1,31 @@
+from functools import wraps
+
+from flask import Blueprint, jsonify
+from flask import request
+
+from ..commands.partner_service import PartnerService
+from ..models.model import init_db, db_session
+from ..models.partner import PartnerJsonSchema, Partner
+
+operations_blueprint = Blueprint('operations', __name__)
+init_db()
+partner_schema = PartnerJsonSchema()
+
+@operations_blueprint.route('/usuarios/consultar-servicios', methods=['GET'])
+def consultar_estatus_microservicios():
+    result = [partner_schema.dump(r) for r in db_session.query(Partner).all()]
+    return jsonify(result), 200
+
+@operations_blueprint.route('/usuarios/crear-servicio', methods=['POST'])
+def create_partner_service():
+    body = request.get_json()
+    message_error, error_post_service = PartnerService.create_partner_service(body)
+    if error_post_service:
+        return message_error, error_post_service
+    else:
+        return 'servicio creado exitoso', 200
+
+
+@operations_blueprint.route('/usuarios/health', methods=['GET'])
+def check_health():
+    return 'ok', 200
